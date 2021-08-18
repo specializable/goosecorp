42b20f1fd98ee2d7efbc8a00b58d963186f7e196
charge <74613350+Phew@users.noreply.github.com>
2021-08-18 18:21:04 -0500

    Delete assets/js directory

--- assets/js/slider.js
+++
@@ -1,9 +0,0 @@
-var video = document.getElementById('myVideo');
-var volumeControl = document.getElementById('vol-control');
-
-var setVolume = function () {
-	video.volume = this.value / 100;
-};
-
-volumeControl.addEventListener('change', setVolume);
-volumeControl.addEventListener('input', setVolume);
